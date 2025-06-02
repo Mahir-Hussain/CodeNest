1f5ce0dc32993277304f59162e9cfcde522a4c06
Mahir-Hussain <145049535+Mahir-Hussain@users.noreply.github.com>
2025-06-02 11:36:02 +0100

    Changed how the passwords are handled and moved functions to correct area

--- login.py
+++ login.py
@@ -7,15 +7,29 @@ class LoginSystem(Database):
     def __init__(self, email, password):
         super().__init__()
         self.email = email
-        self.password = password
+        self.password = self.hash_password(password)
+
+    def hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
 
     def create_user(self):
         try:
             self.cursor.execute(
                 "INSERT INTO users (email, password) VALUES (%s, %s)",
-                (self.email, hashlib.sha256(self.password.encode()).hexdigest()),
-            )
+                (self.email, self.password),
+            ),
+
             self.connection.commit()
             print("User created successfully!")
         except psycopg2.IntegrityError as e:
             print(f"Error: {e}")
+
+    def authenticate(self):
+        self.cursor.execute(
+            "SELECT email, password FROM users WHERE email = %s AND password = %s",
+            (self.email, self.password),
+        )
+        if self.cursor.fetchone():
+            print("Login successful")
+        else:
+            print("Incorrect details")
--- main.py
+++ main.py
@@ -4,20 +4,9 @@ import hashlib
 
 
 if __name__ == "__main__":
-    db = Database()
+    # db = Database()
 
-    def test_login(email, password):
-        db.cursor.execute("SELECT email, password FROM users WHERE email = %s AND password = %s", (email, hashlib.sha256(password.encode()).hexdigest()))
-        if db.cursor.fetchone():
-            print("Login successful")
-        else:
-            print("Incorrect details")
-    
-    p = LoginSystem("outlook", "asdwadw")
-    #p.create_user()
-    test_login("outlook", "asdwadw")
-    db.close()
-
-
-
-    
+    p = LoginSystem("emailtoday", "password123")
+    # p.create_user()
+    p.test_login()
+    # db.close()
