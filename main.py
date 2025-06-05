eed44a3c63f714a6b3e5fbd7704372a4d2cdae94
Samyam <74204487+Titanzz@users.noreply.github.com>
2025-06-05 04:28:45 +0100

    Enable CORS (not 100% sure)

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from backend.database import Database
 from backend.login import LoginSystem
 from backend.snippets import Snippets
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 d = Database()
 l = LoginSystem("GeorgeJoseph@gmail.com", "arjun")
-
-
 
 @app.get("/")
 async def root():
