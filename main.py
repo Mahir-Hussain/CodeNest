6e561882c7541b6bf9d5d52cf1e93348e5d1a591
Mahir-Hussain <145049535+Mahir-Hussain@users.noreply.github.com>
2025-06-07 16:53:36 +0100

    add samyam cors?

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import backend.api
+import backend.api as api
 import uvicorn
 
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000"],  # React dev server
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+api.app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == '__main__':
-    uvicorn.run(backend.api.app, port=8080, host='127.0.0.1')  # Run the FastAPI app on port 8080
+    uvicorn.run(api.app, port=8080, host='127.0.0.1')  # Run the FastAPI app on port 8080
