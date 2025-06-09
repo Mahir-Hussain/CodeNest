ca2f170d1e68416f58e8a876e9a5302df3a5b47d
Mahir-Hussain <145049535+Mahir-Hussain@users.noreply.github.com>
2025-06-09 23:53:43 +0100

    Seperate boxes - need to update visuals

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -38,29 +38,61 @@ function Login(){
     }
   }
 
-  return (
-    <div className="loginContainer">
-      <h1>Login</h1>
-      <form className="form" onSubmit={submitDetails}> 
-        <input 
-          type="email" 
-          placeholder="Email" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
-          required
-        />
-        <input 
-          type="password" 
-          placeholder="Password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          required
-        />
-        <button type="submit">Login</button>
-        <button type="button" onClick={() => navigate("/Signup")}>Sign Up</button>
-      </form>
-    </div>
-  );
+return (
+  <>
+  <div className="loginContainer">
+    <h1>Login</h1>
+    <form className="form" onSubmit={submitDetails}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+
+      <input 
+        type="password" 
+        placeholder="Password" 
+        value={password} 
+        onChange={(event) => setPassword(event.target.value)} 
+        required
+      />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+
+  <div className='signupContainer'>
+    <h6>No account?</h6>
+    <button type="button" onClick={() => navigate("/Signup")}>Sign Up</button>
+  </div>
+  </>
+);
+
+  // return (
+  //   <div className="loginContainer">
+  //     <h1>Login</h1>
+  //     <form className="form" onSubmit={submitDetails}> 
+  //       <input 
+  //         type="email" 
+  //         placeholder="Email" 
+  //         value={email} 
+  //         onChange={(e) => setEmail(e.target.value)} 
+  //         required
+  //       />
+  //       <input 
+  //         type="password" 
+  //         placeholder="Password" 
+  //         value={password} 
+  //         onChange={(e) => setPassword(e.target.value)} 
+  //         required
+  //       />
+  //       <button type="submit">Login</button>
+  //       <button type="button" onClick={() => navigate("/Signup")}>Sign Up</button>
+  //     </form>
+  //   </div>
+  // );
 }
 
 export default Login;
