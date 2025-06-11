5afa5adf898fc46492d0393773278b98b179a6e1
Mahir-Hussain <145049535+Mahir-Hussain@users.noreply.github.com>
2025-06-11 21:02:56 +0100

    Create ai.py

---
+++ backend/ai.py
@@ -0,0 +1,32 @@
+from google import genai
+from secret import ai_key
+
+client = genai.Client(
+    api_key=ai_key
+)  # Get your key from https://ai.google.dev/gemini-api/docs
+
+"""
+PLAN:
+When the user puts their code into the database it should call these functions
+Either:
+- Push code into database then call functions. When they return a response then update in database
+OR:
+- Wait for the ai to respond, then push to database (can take some time)
+"""
+
+
+def get_title(userCode: str):
+    default = "Give me a short title for this code. Do not deviate."
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=f"{default}, [{userCode}]",
+    )
+    return response
+
+
+def get_language(userCode: str):
+    default = "In one word, tell me what programming language the user is using. Do not deviate."
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=f"{default}, [{userCode}]"
+    )
+    return response
